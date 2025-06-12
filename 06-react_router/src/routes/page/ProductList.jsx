4c77b2ba19b37ab7e4d8939a4f8bb9ab10a7c0c0
김승혁 <peter9889466@gmail.com>
2025-06-13 08:42:47 +0900

    productlist

--- 06-react_router/src/routes/page/ProductList.jsx
+++ 06-react_router/src/routes/page/ProductList.jsx
@@ -9,6 +9,7 @@ const ProductList = () => {
     return (
         <div>
             <h1>ProductList 페이지</h1>
+            <p className='marginTop:50px'>gkdkdkdkaslejkga</p>
             <button onClick={goHome}>Home 이동</button>
         </div>
     )
